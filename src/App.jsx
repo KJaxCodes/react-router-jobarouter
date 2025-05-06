3be882ac6b7f01e5f1090eed08a86f0d3a17075c
Kelly J <[kelly4410@gmail.com]>
2025-05-06 16:31:39 -0600

    add careers

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,49 @@
-import './App.css'
-
 import {
   createBrowserRouter,
-  Route,
   createRoutesFromElements,
+  Route,
   RouterProvider
-} from 'react-router-dom';
+} from 'react-router-dom'
 
-//pages
-import Home from './pages/Home';
-import About from './pages/About';
-import FAQ from './pages/help/FAQ';
-import Contact from './pages/help/Contact';
-import NotFound from './pages/NotFound';
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import FAQ from './pages/help/FAQ'
+import Contact from './pages/help/Contact'
+import NotFound from './pages/NotFound'
+import Careers, { careersLoader } from './pages/careers/Careers'
 
-//layouts
-import RootLayout from './layouts/RootLayout';
-import HelpLayout from './layouts/HelpLayout';
+// layouts
+import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import CareersLayout from './layouts/CareersLayout'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      {/* <Route path="/" element={<Home />} /> */}
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<FAQ />} />
         <Route path="contact" element={<Contact />} />
+      </Route>
+      <Route path="careers" element={<CareersLayout />}>
+        <Route
+          index
+          element={<Careers />}
+          loader={careersLoader}
+        />
       </Route>
 
       <Route path="*" element={<NotFound />} />
     </Route>
   )
 )
+
 function App() {
   return (
     <RouterProvider router={router} />
   );
 }
 
-export default App;
+export default App
