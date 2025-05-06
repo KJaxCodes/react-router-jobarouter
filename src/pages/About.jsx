d62dfc6fcc1adfb5caa89913d4ccb4ef89da1e67
Kelly J <[kelly4410@gmail.com]>
2025-05-06 12:46:24 -0600

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div className="about">
+            <h2>About Us</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio asperiores dolores quia obcaecati aliquam! Omnis, modi at? Accusamus obcaecati explicabo ut. Atque iure error quibusdam rerum architecto, culpa placeat quo!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dignissimos tempora ipsam facilis facere sunt quaerat in! Nesciunt aspernatur, sequi molestias amet expedita illo inventore architecto at quasi quas aperiam?</p>
+        </div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam dolorem quidem aperiam ex sed doloremque illum. Officia delectus, inventore aliquam esse voluptate ex, eligendi optio dolor veritatis fugit totam quae.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad ipsa ut quisquam nobis. Alias, pariatur culpa quis cupiditate aliquid nobis tempore error consequatur quos voluptas quisquam saepe maiores rerum quas.
+                Corrupti nostrum, dolores sit quibusdam consequatur aspernatur dolor nam ullam exercitationem in, eius porro non quo, asperiores ipsa explicabo est doloremque distinctio. Id beatae sint fugit vitae et. Ipsam, fugit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, aliquid accusantium. Modi ad itaque aliquam dolorem doloribus numquam pariatur alias saepe, provident accusamus nam vitae soluta molestiae, aliquid nulla quas!</p>
+        </div>
+    )
+}
